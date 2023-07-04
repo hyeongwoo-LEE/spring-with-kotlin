61816f438c4204cfaf2e05660115e9dfa08636ac
browoo <leehw@gabia.com>
2023-07-04 10:42:01 +0900

    Initial commit

---
+++ src/main/kotlin/com/group/libraryapp/domain/book/Book.kt
@@ -0,0 +1,36 @@
+package com.group.libraryapp.domain.book
+
+import javax.persistence.Entity
+import javax.persistence.GeneratedValue
+import javax.persistence.GenerationType
+import javax.persistence.Id
+
+@Entity
+class Book (
+        val name: String,
+
+        val type: String,
+
+        @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+        val id: Long? = null
+) {
+    init {
+        if (name.isBlank()) {
+            throw IllegalArgumentException("이름은 비어 있을 수 없습니다.")
+        }
+    }
+
+    companion object {
+        fun fixture(
+                name: String = "책 이름",
+                type: String = "COMPUTER",
+                id: Long? = null
+        ): Book {
+            return Book(
+                    name = name,
+                    type = type,
+                    id = id
+            )
+        }
+    }
+}
---
+++ src/test/java/com/group/libraryapp/service/book/BookServiceTest.kt
@@ -0,0 +1,27 @@
+package com.group.libraryapp.service.book
+
+import com.group.libraryapp.domain.book.BookRepository
+import com.group.libraryapp.domain.user.UserRepository
+import com.group.libraryapp.domain.user.loanHistory.UserLoanHistoryRepository
+import org.junit.jupiter.api.AfterEach
+import org.junit.jupiter.api.Assertions.*
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class BookServiceTest @Autowired constructor(
+        private val bookService: BookService,
+        private val bookRepository: BookRepository,
+        private val userRepository: UserRepository,
+        private val userLoanHistoryRepository: UserLoanHistoryRepository
+) {
+
+    @AfterEach
+    fun clean() {
+        bookRepository.deleteAll()
+        userRepository.deleteAll()
+    }
+
+
+
+}
